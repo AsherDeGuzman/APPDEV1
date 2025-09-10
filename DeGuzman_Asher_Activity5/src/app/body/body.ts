68b851cd770b5fe30dcc3acb456de4258ffcc6e5
Asher De Guzman <de guzman_a@ca01.ub.lan>
2025-09-10 13:08:53 +0800

    Added Activity 4(Color Picker) and Activity 5(Counter)

---
+++ DeGuzman_Asher_Activity5/src/app/body/body.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  standalone: false,
+  templateUrl: './body.html',
+  styleUrl: './body.css'
+})
+export class Body {
+  count: number = 0;
+  
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    this.count--;
+  }
+
+  reset() {
+    this.count = 0;
+  } 
+
+  random() {
+    if (this.count > 0) {
+      this.count = Math.floor(Math.random() * -100);
+    } else {
+      this.count = Math.floor(Math.random() * 100);
+    }
+  }
+}
